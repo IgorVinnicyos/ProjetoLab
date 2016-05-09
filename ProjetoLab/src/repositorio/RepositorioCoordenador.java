0a8b04cd9c9f62970d6ecba8c38cd0a21e232201
J.Anderson <J.Anderson@Anderson.localdomain>
2016-05-09 14:47:24 -0300

    Começando repositorio Coordenador

---
+++ ProjetoLab/src/repositorio/RepositorioCoordenador.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositorio;
+
+import classes.Coordenador;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author J.Anderson
+ */
+public class RepositorioCoordenador {
+    private static ArrayList<Coordenador> listaCoordenador = new ArrayList();
+
+    public static void RepositorioCoordenador() {
+        
+    }
+
+   
+    
+    
+    
+}
